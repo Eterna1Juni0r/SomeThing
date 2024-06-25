6b164b0a8179fe8d22c4ef3d32db7080b6aab99b
Eterna1Juni0r <sergey7690@mail.ru>
2024-06-25 15:49:01 +0300

    FLeX

--- src/pages/CalculatorPage.vue
+++ src/pages/CalculatorPage.vue
@@ -8,6 +8,7 @@
 
 <style lang="scss" scoped>
 .panel {
+  display: flex;
   margin-top: -9%;
   padding: 16px;
   color: white;
@@ -15,5 +16,7 @@
   height: 640px;
   background: #2b2323;
   border-radius: 20px;
+  align-items: center;
+  justify-content: center;
 }
 </style>
