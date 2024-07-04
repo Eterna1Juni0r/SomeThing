bd6134546ca9fe83286a2d0ba017f2e083612944
Eterna1Juni0r <sergey7690@mail.ru>
2024-07-04 23:41:50 +0300

    debug

--- src/pages/CalculatorPage.vue
+++ src/pages/CalculatorPage.vue
@@ -2,7 +2,7 @@
   <h1 class="head">CRYPTO</h1>
 
   <div class="base">
-    <h1>Почему-то не WORK SUKA BLYAT</h1>
+    <h1>Почему-то не WORKaet SUKA BLYAT</h1>
     <CryptoInput :changeAmount="changeAmount" :convert="convert" />
     <p v-if="error">{{ error }}</p>
     <p v-if="result !== 0">{{ result }}</p>
@@ -24,13 +24,13 @@ const amount = ref(1);
 const cryptoFirst = ref("");
 const cryptoSecond = ref("");
 const error = ref("");
-const result = ref(0);
+const result = ref(1);
 
 const setCryptoSecond = (val) => {
   cryptoSecond.value = val;
 };
 
-const convert = async () => {
+async function convert() {
   if (amount.value <= 0) {
     error.value = "Введите число больше 0";
     return;
@@ -45,20 +45,27 @@ const convert = async () => {
 
   await converty.ready();
 
-  if (cryptoFirst.value == "BTS" && cryptoSecond == "ETH") {
+  console.log(cryptoFirst.value);
+  console.log(cryptoSecond.value);
+
+  if (cryptoFirst.value == "BTC" && cryptoSecond.value == "ETH") {
+    console.log("Да выполнено");
     result.value = converty.BTC.ETH(amount.value);
-  } else if (cryptoFirst.value == "BTS" && cryptoSecond == "USDT") {
+  } else if (cryptoFirst.value == "BTC" && cryptoSecond.value == "USDT") {
     result.value = converty.BTC.USDT(amount.value);
-  } else if (cryptoFirst.value == "ETH" && cryptoSecond == "USDT") {
+  } else if (cryptoFirst.value == "ETH" && cryptoSecond.value == "USDT") {
     result.value = converty.ETH.USDT(amount.value);
-  } else if (cryptoFirst.value == "ETH" && cryptoSecond == "BTS") {
-    result.value = converty.ETH.BTS(amount.value);
-  } else if (cryptoFirst.value == "USDT" && cryptoSecond == "ETH") {
+  } else if (cryptoFirst.value == "ETH" && cryptoSecond.value == "BTC") {
+    result.value = converty.ETH.BTC(amount.value);
+  } else if (cryptoFirst.value == "USDT" && cryptoSecond.value == "ETH") {
     result.value = converty.USDT.ETH(amount.value);
-  } else if (cryptoFirst.value == "USDT" && cryptoSecond == "BTS") {
-    result.value = converty.USDT.BTS(amount.value);
+  } else if (cryptoFirst.value == "USDT" && cryptoSecond.value == "BTC") {
+    result.value = converty.USDT.BTC(amount.value);
   }
-};
+
+  console.log(cryptoFirst.value);
+  console.log(cryptoSecond.value);
+}
 const setCryptoFirst = (val) => {
   cryptoFirst.value = val;
 };
